0a4aadbca1b38a775db5add30931f5ff5278c400
Bhagyesh <wifysystemsmanufacturer@gmail.com>
2024-12-12 16:12:29 +0530

    setup1

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.post('/data', (req, res) => {
-  console.log(req.body);
-  res.send('Data received');
+app.post("/login", async (req, res) => {
+  const accessToken = req.body.accessToken;
+
+  try {
+    const response = await axios.get(
+      `https://graph.facebook.com/me?access_token=${accessToken}&fields=id,name,email`
+    );
+    const userData = response.data;
+    res.json(userData);
+  } catch (error) {
+    console.error("Error fetching Facebook data:", error);
+    res.status(500).send("Error fetching data from Facebook");
+  }
 });
 
 const PORT = 3000;
