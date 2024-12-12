88db2751bbfc789eacc538085af4ceb198d29041
Bhagyesh <wifysystemsmanufacturer@gmail.com>
2024-12-12 16:29:33 +0530

    setup

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
-const express = require("express");
-
+const express = require('express');
+const axios = require('axios');
+const path = require('path');
 const app = express();
 
-app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.post("/login", async (req, res) => {
-  const accessToken = req.body.accessToken;
+app.get('/auth/facebook/callback', async (req, res) => {
+    const accessToken = req.query.access_token;
 
-  try {
-    const response = await axios.get(
-      `https://graph.facebook.com/me?access_token=${accessToken}&fields=id,name,email`
-    );
-    const userData = response.data;
-    res.json(userData);
-  } catch (error) {
-    console.error("Error fetching Facebook data:", error);
-    res.status(500).send("Error fetching data from Facebook");
-  }
+    if (!accessToken) {
+        return res.status(400).send('Missing access token');
+    }
+
+    try {
+        const response = await axios.get(`https://graph.facebook.com/me?access_token=${accessToken}&fields=id,name,email`);
+        const userData = response.data;
+
+        res.json(userData);
+    } catch (error) {
+        console.error('Error fetching data from Facebook:', error);
+        res.status(500).send('Error fetching data from Facebook');
+    }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
